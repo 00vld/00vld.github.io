138c8319766bd2525e9114dc65f01d65536a3416
00vld <hackurr@proton.me>
2026-02-17 08:36:28 +0530

    First Build

--- js/main.js
+++ js/main.js
@@ -9,79 +9,74 @@ $(document).ready(function () {
   const $panel = $('.panel-cover')
   const $content = $('.content-wrapper')
 
-  // Detect homepage - must check BEFORE any operations
-  const currentPath = window.location.pathname.replace(/\/$/, '') || '/'
-  const basePath = '{{ site.baseurl }}'.replace(/\/$/, '') || '/'
-  const isHomePage = (currentPath === basePath || currentPath === basePath + '/index.html')
-
-  console.log('DEBUG:', { currentPath, basePath, isHomePage })
-
   $('body').addClass('page-loaded')
 
   {% if site.disable_landing_page != true %}
 
-  // If NOT on homepage, auto-collapse sidebar
-  if (!isHomePage) {
+  // Check if we're on homepage
+  const pathname = window.location.pathname
+  const isHome = (
+    pathname === '{{ site.baseurl }}/' ||
+    pathname === '{{ site.baseurl }}/index.html' ||
+    pathname === '/' ||
+    pathname === '/index.html'
+  )
+
+  // Auto-collapse if not on homepage OR if #blog hash is present
+  if (!isHome || window.location.hash === '#blog') {
     $panel.addClass('panel-cover--collapsed')
   }
 
-  // Handle blog button clicks
+  // Blog button click handler
   $('a.blog-button').on('click', function (e) {
-    e.preventDefault()
-    e.stopPropagation()
+    const href = $(this).attr('href')
+    const $this = $(this)
+    
+    console.log('Click:', { href, isHome, currentPath: pathname })
 
-    const $link = $(this)
-    const targetUrl = $link.attr('href')
-    const normalizedTarget = targetUrl.replace(/\/$/, '') || '/'
-
-    console.log('Button clicked:', { targetUrl, normalizedTarget, currentPath, isHomePage })
-
-    // CASE 1: We're on homepage (full cover showing)
-    if (isHomePage) {
+    // CASE 1: Clicking "About" button (href contains #blog)
+    if (href.indexOf('#blog') !== -1) {
       
-      // Don't do anything if clicking "About" while already on homepage
-      if (normalizedTarget === basePath || normalizedTarget === '/') {
-        console.log('Already on homepage - ignoring click')
+      // If we're on homepage with full cover, collapse it
+      if (isHome && !$panel.hasClass('panel-cover--collapsed')) {
+        e.preventDefault()
+        $panel.addClass('panel-cover--collapsed')
+        $content.addClass('animated slideInRight')
         return
       }
+      
+      // If we're on another page, go to homepage and collapse
+      if (!isHome) {
+        e.preventDefault()
+        window.location.href = '{{ site.baseurl }}/#blog'
+        return
+      }
+      
+      // Already collapsed on homepage, do nothing
+      return
+    }
 
-      // Clicking Notes/other page from homepage
-      console.log('Navigating from homepage to:', targetUrl)
+    // CASE 2: Clicking "Notes" or other page
+    e.preventDefault()
+    
+    // If on homepage with full cover, animate then navigate
+    if (isHome && !$panel.hasClass('panel-cover--collapsed')) {
       $panel.addClass('panel-cover--collapsed')
       $content.addClass('animated slideInRight')
       
       setTimeout(function () {
-        window.location.href = targetUrl
+        window.location.href = href
       }, 400)
-      
       return
     }
-
-    // CASE 2: We're on an internal page (About/Notes)
     
-    // If clicking About/homepage link, go back to homepage
-    if (normalizedTarget === basePath || normalizedTarget === '/' || normalizedTarget === '') {
-      console.log('Going back to homepage')
-      window.location.href = basePath || '/'
-      return
-    }
-
-    // If clicking Notes while already on Notes, go back to homepage
-    if (normalizedTarget === currentPath) {
-      console.log('Same page clicked - going to homepage')
-      window.location.href = basePath || '/'
-      return
-    }
-
-    // Otherwise, navigate to the target page
-    console.log('Navigating to:', targetUrl)
-    window.location.href = targetUrl
-
+    // Otherwise just navigate
+    window.location.href = href
   })
 
   {% endif %}
 
-  // Mobile menu toggle
+  // Mobile menu
   $('.btn-mobile-menu').on('click', function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
     $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
